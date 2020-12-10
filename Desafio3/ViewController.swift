17bca6e1dba5ef41c31fdc2b69ea2c90917cd970
Rodrigo Lemos <rodlemoss@gmail.com>
2020-12-10 16:41:10 -0400

    setup

--- Desafio3/ViewController.swift
+++ Desafio3/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var downloadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupButton()
     }
 
+    @IBAction func downloadApi(_ sender: Any) {
+        print("kkk")
+    }
+}
 
+extension ViewController {
+    
+    func setupButton() {
+        downloadButton.backgroundColor = .clear
+        downloadButton.layer.cornerRadius = 5
+        downloadButton.layer.borderWidth = 2
+        downloadButton.layer.borderColor = UIColor.orange.cgColor
+        downloadButton.contentEdgeInsets = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    }
 }
 
